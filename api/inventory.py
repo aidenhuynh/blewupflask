94ed9c3f99594af6e412698bcb2db3693713639b
SamitPoojary <samitpoojary@gmail.com>
2023-01-22 23:51:24 -0800

    idk but plz work

--- api/inventory.py
+++ api/inventory.py
@@ -1,84 +1,45 @@
-from flask import Flask, request, jsonify
-from flask_restful import Api, Resource
-from datetime import datetime
-from model.users import User
+from flask import Flask, request
 from .. import db
-from model.inventory import Inventory
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class UserAPI(Resource):
-    def post(self):
-        ''' Read data for json body '''
-        body = request.get_json()
-            
-        ''' Avoid garbage in, error checking '''
-        # validate name
-        name = body.get('name')
-        if name is None or len(name) < 2:
-            return {'message': f'Name is missing, or is less than 2 characters'}, 210
-        # validate uid
-        uid = body.get('uid')
-        if uid is None or len(uid) < 2:
-            return {'message': f'User ID is missing, or is less than 2 characters'}, 210
-        # look for password and dob
-        password = body.get('password')
-        dob = body.get('dob')
-
-        ''' #1: Key code block, setup USER OBJECT '''
-        uo = User(name=name, 
-                  uid=uid)
-            
-        ''' Additional garbage error checking '''
-        # set password if provided
-        if password is not None:
-            uo.set_password(password)
-        # convert to date type
-        if dob is not None:
-            try:
-                uo.dob = datetime.strptime(dob, '%m-%d-%Y').date()
-            except:
-                return {'message': f'Date of birth format error {dob}, must be mm-dd-yyyy'}, 210
-            
-        ''' #2: Key Code block to add user to database '''
-        # create user in database
-        user = uo.create()
-        # success returns json of user
-        if user:
-            return jsonify(user.read())
-        # failure returns error
-        return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 210
-
 class InventoryAPI(Resource):
     def post(self):
         body = request.get_json()
-
         date = body.get('date')
         action = body.get('action')
         user = body.get('user')
         item = body.get('item')
         quantity = body.get('quantity')
 
-class InventoryAPI(Resource):
-    def post(self):
-        body = request.get_json()
+        # Create a new entry in the database
+        new_entry = {'date': date, 'action': action, 'user': user, 'item': item, 'quantity': quantity}
+        inventory_list.append(new_entry)
 
+        return {'message': 'Inventory entry added successfully'}, 201
+
+    def get(self):
+        return {'inventory': inventory_list}
+
+    def put(self, id):
+        body = request.get_json()
         date = body.get('date')
         action = body.get('action')
         user = body.get('user')
         item = body.get('item')
         quantity = body.get('quantity')
 
-        if date is None or action is None or user is None or item is None or quantity is None:
-            return {'message': 'Missing one or more required fields'}, 210
+        # Update the entry in the database
+        inventory_list[id] = {'date': date, 'action': action, 'user': user, 'item': item, 'quantity': quantity}
 
-        new_inventory = Inventory(date=date, action=action, user=user, item=item, quantity=quantity)
-        db.session.add(new_inventory)
-        db.session.commit()
+        return {'message': 'Inventory entry updated successfully'}
 
-        return {'message': 'Inventory added successfully'}, 200
+    def delete(self, id):
+        # Delete the entry from the database
+        del inventory_list[id]
 
-# Adding some sample data to the Inventory table
+        return {'message': 'Inventory entry deleted successfully'}
 
-api.add_resource(InventoryAPI, '/inventory')
+api.add_resource(InventoryAPI, '/inventory/<int:id>')
