6eb7665300ddb625861438b82aca829bffb4446e
jeffrey <leejeffreysc@gmail.com>
2023-02-03 12:07:04 -0800

    the feature part is basically done

--- tracker.py
+++ tracker.py
@@ -2,13 +2,23 @@ import phonenumbers
 
 from text import number
 from phonenumbers import geocoder
+from phonenumbers import carrier
+from phonenumbers import timezone  
+
+print("Inputted Phone Number:", number + "\n")
 
 ch_number = phonenumbers.parse(number, "CH")
-print("Current Location:")
-print(geocoder.description_for_number(ch_number, "en"))
-print(" ")
+print("Current Location:", geocoder.description_for_number(ch_number, "en") + "\n")
+
+givenPN = phonenumbers.parse(number, "CH")  
+# Using parse phone number for finding timezone  
+timezoneOfPN = timezone.time_zones_for_number(givenPN)  
+# Printing carrier as the result  
+print("The timezone of the given phone number is:", timezoneOfPN, "\n") 
 
 import datetime
 now = datetime.datetime.now()
-print ("Current date and time : ")
-print (now.strftime("%Y-%m-%d %H:%M:%S"))
+print ("The current date and time is:", now.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+
