87ce286fba4376db958753209ae87a7b9af96a3c
jeffrey <leejeffreysc@gmail.com>
2023-02-15 00:42:58 -0800

    maybe, we will hope

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from flask_cors import CORS
+import threading
+
+# import "packages" from flask
+from flask import render_template  # import render_template from "public" flask libraries
+
+# import "packages" from "this" project
+from __init__ import app  # Definitions initialization
+
+# setup App pages
+from projects.projects import app_projects # Blueprint directory import projects definition
+
 from api import app, db
 
 from api.inventory import inventory_bp
@@ -7,7 +17,21 @@ from model.inventory import init_inventory
 
 app.register_blueprint(inventory_bp)
 
+# register URIs
+app.register_blueprint(covid_api) # register api routes
 
+@app.errorhandler(404)  # catch for URL not found
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+@app.route('/')  # connects default URL to index() function
+def index():
+    return render_template("index.html")
+
+@app.route('/stub/')  # connects /stub/ URL to stub() function
+def stub():
+    return render_template("stub.html")
 
 @app.before_first_request
 def init_db():
@@ -17,5 +41,6 @@ def init_db():
 
 if __name__ == "__main__":
     # change name for testing
+    from flask_cors import CORS
     cors = CORS(app)
     app.run(debug=True, host="0.0.0.0", port="8086")
