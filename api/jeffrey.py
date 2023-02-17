2b063d60ee7e4bce31586c6a27c50d8615d62aeb
jeffrey <leejeffreysc@gmail.com>
2023-02-16 17:26:51 -0800

    updates to backend?

--- api/jeffrey.py
+++ api/jeffrey.py
@@ -1,19 +1,21 @@
-from flask import Flask, request, jsonify
-import phonenumbers
+from flask import Flask, render_template, request, jsonify
 import sqlite3
-from phonenumbers import geocoder
-from phonenumbers import timezone
+import phonenumbers
+from phonenumbers import geocoder, carrier, timezone
 from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/lookup', methods=['POST'])
-def lookup():
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
     user_id = request.form['user_id']
     phone_number = request.form['phone_number']
 
     givenPN = phonenumbers.parse(phone_number, "CH")
-
     # get location
     ch_number = phonenumbers.parse(phone_number, "CH")
     location = geocoder.description_for_number(ch_number, "en")
@@ -25,13 +27,17 @@ def lookup():
     now = datetime.now()
     time = now.strftime("%Y-%m-%d %H:%M:%S")
 
-    # connect to database
-    conn = sqlite3.connect('sqlites.db')
-    cursor = conn.cursor()
+    ch_number = phonenumbers.parse(phone_number, "CH")
+    location = geocoder.description_for_number(ch_number, "en")
+    givenPN = phonenumbers.parse(phone_number, "CH")
+    timezoneOfPN = timezone.time_zones_for_number(givenPN)
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    timezone1 = timezoneOfPN[0] if len(timezoneOfPN) > 0 else ""
 
-    # insert data into database
-    cursor.execute("INSERT INTO phone (user_id, phone_number, location, timezone, time) VALUES (?, ?, ?, ?, ?)",
-                   (user_id, phone_number, location, timezoneOfPN[0], time))
+    # connect to database
+    conn = sqlite3.connect('api/sqlites.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO phone VALUES (?, ?, ?, ?, ?)", (user_id, phone_number, location, timezone1, time))
     conn.commit()
     conn.close()
 
@@ -39,7 +45,7 @@ def lookup():
 
 @app.route('/api/phone')
 def get_phone_data():
-    conn = sqlite3.connect('sqlites.db')
+    conn = sqlite3.connect('api/sqlites.db')
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM phone")
     data = cursor.fetchall()
