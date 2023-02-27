82df9d46b51b94180fe11eaf12ef56e09fd30aa8
jeffrey <leejeffreysc@gmail.com>
2023-02-26 23:21:32 -0800

    yessir

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import threading
 # import "packages" from flask
 from flask import render_template  # import render_template from "public" flask libraries
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import sqlite3
+import phonenumbers
+from phonenumbers import geocoder, timezone
+import datetime
 from flask_cors import CORS
 # import "packages" from "this" project
 from __init__ import app, db  # Definitions initialization
@@ -12,6 +15,8 @@ from projects.projects import app_projects # Blueprint directory import projects
 from model.users import initUsers
 from api.user import user_api
 from model.inventory import init_inventories
+
+
 
 from api.inventory import inventories_bp
 app.register_blueprint(user_api)
@@ -58,6 +63,29 @@ def phone():
         })
 
     return jsonify(data)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    user_id = request.form['user_id']
+    number = request.form['phone_number']
+
+    # Run algorithm to find location and timezone of phone number
+    ch_number = phonenumbers.parse(number, "CH")
+    location = geocoder.description_for_number(ch_number, "en")
+    givenPN = phonenumbers.parse(number, "CH")
+    timezoneOfPN = timezone.time_zones_for_number(givenPN)
+    timezone1 = timezoneOfPN[0] if timezoneOfPN else None
+    now = datetime.datetime.now()
+    time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Save the data to the database
+    conn = sqlite3.connect('api/sqlites.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO phone (user_id, phone_number, location, timezone, time) VALUES (?, ?, ?, ?, ?)", (user_id, number, location, timezone1, time))
+    conn.commit()
+    conn.close()
+
+    return "Data has been submitted successfully."
 
 if __name__ == "__main__":
     db.init_app(app)
