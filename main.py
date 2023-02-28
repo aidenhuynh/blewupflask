84d351c27ac2962761cbc25bb47d7e64711c399a
Aiden Huynh <ah5993909@gmail.com>
2023-02-28 12:41:09 -0800

    sdasdasdas

--- main.py
+++ main.py
@@ -97,10 +97,11 @@ if __name__ == "__main__":
     
 toptext = Flask(__name__)
 
-toptext.config['CORS_ALLOW_HEADERS'] = ['Content-Type']
+toptext.config['CORS_ALLOW_HEADERS'] = ['Content-Type', 'Access-Control-Allow-Private-Network', 'Access-Control-Request-Private-Network']
 toptext.config['CORS_METHODS'] = ['PUT', 'GET', 'REMOVE', 'PATCH']
-toptext.config['CORS_HEADERS'] = ['Access-Control-Allow-Origin: http://0.0.0.0/4002/leuckblewup', 'Access-Control-Allow-Private-Network: true', 'Access-Control-Request-Private-Network: true']
-toptext.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin: https://aidenhuynh.github.io/leuckblewup'
+toptext.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin: *'
+
+# toptext.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin: https://aidenhuynh.github.io/leuckblewup'
 
 # attention gamers, run the following command in terminal
 # pip install Flask-Cors
